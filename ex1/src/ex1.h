a2207907428bb6a4efc19d6937f75f9610465aeb
Eran Weissenster <eranws@cs.huji.ac.il>
2013-10-28 16:15:09 +0200

    fixed ex0->ex1

---
+++ ex1/src/ex1.h
@@ -0,0 +1,13 @@
+//
+//  ex1.h
+//  cg-projects
+//
+//  Created by HUJI Computer Graphics course staff, 2013.
+//
+
+#ifndef __ex1__ex1__
+#define __ex1__ex1__
+
+#include <iostream>
+
+#endif /* defined(__ex1__ex1__) */
